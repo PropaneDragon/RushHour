71ca9092d404af8ff651145d083c0ae23abfe20e
Jasmine Hegman <jasmine@jhegman.com>
2016-03-03 21:44:40 -0700

    Started TimeOfDaySliderTests

---
+++ RushHourTests/MockClasses/UnityEngineOverrides.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RushHourTests.MockClasses
+{
+    // Idea borrowed from http://forum.unity3d.com/threads/security-exception-ecall-methods-must-be-packaged-into-a-system-module.98230/
+    // See also http://forum.unity3d.com/threads/networkserver-in-console-application.362012/
+    // and http://forum.unity3d.com/threads/c-console-server-ecall-error.50163/
+    // http://www.amitloaf.com/2014/02/continuous-integration-and-unit-tests_12.html
+    // and http://stackoverflow.com/questions/11286004/securityexception-ecall-methods-must-be-packaged-into-a-system-module
+    // and http://answers.unity3d.com/questions/628962/why-cant-we-use-unityenginedll-outside-of-unity.html
+    // :(
+    public class Debug
+    {
+        public static void Log(string s) { Console.WriteLine(s); }
+        public static void LogWarning(string s) { Console.WriteLine(s); }
+        public static void LogError(string s) { Console.WriteLine(s); }
+    }
+
+    public class MonoBehaviour
+    {
+
+    }
+}
