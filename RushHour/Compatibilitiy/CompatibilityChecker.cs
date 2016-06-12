b898e857f73668572aa1d0963b4fda342d394f45
Sean <thingshappen.xuras@gmail.com>
2016-06-12 10:34:18 +0100

    Added a mod compatibility checker

---
+++ RushHour/Compatibilitiy/CompatibilityChecker.cs
@@ -0,0 +1,99 @@
+﻿using ColossalFramework;
+using ColossalFramework.Plugins;
+using ColossalFramework.Steamworks;
+using ColossalFramework.UI;
+using ICities;
+using System.Collections.Generic;
+
+namespace RushHour.Compatibilitiy
+{
+    internal class CompatibilityChecker
+    {
+        private static CompatibilityChecker _instance = null;
+        public static CompatibilityChecker Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new CompatibilityChecker();
+                }
+
+                return _instance;
+            }
+        }
+
+        private Dictionary<PublishedFileId, string> _incompatibilities = new Dictionary<PublishedFileId, string>()
+        {
+            {
+                new PublishedFileId(524021335ul),
+                "The event blip system for the new football events doesn't work with {0}. " +
+                "Use the time progression controls in the options for Rush Hour instead. " +
+                "Compatibility might be possible in the future, but only if you really need it. Let me know."
+            }
+        };
+
+        public Dictionary<PublishedFileId, string> CheckIncompatibilities()
+        {
+            var userIncompatibilities = new Dictionary<PublishedFileId, string>();
+
+            foreach(PublishedFileId publishedFile in Steam.workshop.GetSubscribedItems())
+            {
+                if(_incompatibilities.ContainsKey(publishedFile))
+                {
+                    userIncompatibilities.Add(publishedFile, _incompatibilities[publishedFile]);
+                }
+            }
+
+            return userIncompatibilities;
+        }
+
+        public void DisplayIncompatibleMods()
+        {
+            Dictionary<PublishedFileId, string> userIncompatibilities = CheckIncompatibilities();
+
+            if(userIncompatibilities.Count > 0)
+            {
+                PluginManager pluginManager = Singleton<PluginManager>.instance;
+                var plugins = pluginManager.GetPluginsInfo();
+                string incompatibleList = "";
+
+                foreach(KeyValuePair<PublishedFileId, string> incompatibility in userIncompatibilities)
+                {
+                    PluginManager.PluginInfo foundPlugin = null;
+                    string pluginName = "Unknown mod (" + incompatibility.Key.AsUInt64.ToString() + ")";
+
+                    foreach(PluginManager.PluginInfo plugin in plugins)
+                    {
+                        if(plugin.publishedFileID == incompatibility.Key)
+                        {
+                            foundPlugin = plugin;
+                            break;
+                        }
+                    }
+
+                    if (incompatibleList != "")
+                    {
+                        incompatibleList += "\n\n";
+                    }
+
+                    if (foundPlugin != null)
+                    {
+                        IUserMod[] instances = foundPlugin.GetInstances<IUserMod>();
+
+                        if(instances.Length > 0)
+                        {
+                            pluginName = instances[0].Name;
+                        }
+                    }
+
+                    incompatibleList += pluginName + " - " + string.Format(incompatibility.Value, pluginName);
+                }
+
+                incompatibleList += "\n\nYou can still run the game with th" + (userIncompatibilities.Count > 1 ? "ese mods" : "is mod") + ", but some things in Rush Hour might not work.";
+
+                UIView.library.ShowModal<ExceptionPanel>("ExceptionPanel").SetMessage("Rush Hour has detected incompatible mods", incompatibleList, false);
+            }
+        }
+    }
+}
--- RushHour/RushHourMod.cs
+++ RushHour/RushHourMod.cs
@@ -1,5 +1,9 @@
-﻿using ICities;
+﻿using ColossalFramework;
+using ICities;
+using RushHour.Compatibilitiy;
+using RushHour.Experiments;
 using RushHour.Options;
+using UnityEngine;
 
 namespace RushHour
 {
@@ -7,6 +11,23 @@ namespace RushHour
     {
         public string Name => "Rush Hour";
         public string Description => "Improves AI so citizens and tourists act more realistically.";
+
+        public void OnEnabled()
+        {
+            Debug.Log("Rush Hour has been enabled.");
+            DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "Rush Hour has been enabled.");
+
+            Singleton<LoadingManager>.Ensure();
+            Singleton<LoadingManager>.instance.m_introLoaded += OnIntroLoaded;
+        }
+
+        private void OnIntroLoaded()
+        {
+            if (ExperimentsToggle.ShowIncompatibleMods)
+            {
+                CompatibilityChecker.Instance.DisplayIncompatibleMods();
+            }
+        }
 
         public void OnSettingsUI(UIHelperBase helper)
         {
